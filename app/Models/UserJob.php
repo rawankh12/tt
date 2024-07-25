4e2254c2376f17e85f5616214eac27ba7eb0b6b3
rawan <rawan@outlook.com>
2024-07-26 02:33:07 +0300

    comment

---
+++ app/Models/UserJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserJob extends Model
+{
+    use HasFactory;
+
+    protected $table='jobs';
+
+    protected $fillable = [
+        'user_id',
+        'job_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class, 'job_id');
+    }
+
+}
